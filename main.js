9349e1a1bb101968d4f402ea7ed4e6194a4bc30e
felix <felix.rauch@student.uibk.ac.at>
2022-05-19 17:33:37 +0200

    Temepratur Label mittig positionieren

--- main.js
+++ main.js
@@ -99,6 +99,7 @@ let drawTemperature = function(geojson) {
                 geoJsonPoint.properties.LT,
                 COLORS.temperature
             )
+            
             return L.marker(latlng, {
                 icon: L.divIcon({
                     className: "aws-div-icon",
